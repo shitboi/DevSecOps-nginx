be50b4ad33323e594bdaab8e0af342f2e3157067
ayoshittu <55026063+shitboi@users.noreply.github.com>
2025-12-16 09:33:11 -0500

    Update CICD.sh

--- CICD.sh
+++ CICD.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
 
-APP_NAME=mywebapp
-VERSION=INSECURE
+APP_NAME=vul-mywebapp
+VERSION=STILL-INSECURE
 
 #BUILD: Clenaup old container/build image
 docker rm -f $APP_NAME 2>/dev/null || true
-docker build -t $APP_NAME:$VERSION . 
+docker build -t $APP_NAME:$VERSION .
+
+#SCA Scanning
+docker scout cves $APP_NAME:$VERSION --output ./vulns.report
+docker scout cves $APP_NAME:$VERSION --only-severity critical --exit-code
+docker scout sbom --output $APP_NAME.sbom $APP_NAME:$VERSION
 
 #TEST: Run the container
-docker run -d -p 80:80 --name $APP_NAME:$VERSION webapp
+docker run -d -p 800:80 --name webapp $APP_NAME:$VERSION
 
 
